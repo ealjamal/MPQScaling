f36c1c218a780bbe6fe7e2824b552605deadef61
Eddie Aljamal <aljamaleddie@gmail.com>
2025-05-15 19:23:50 -0400

    changhange save path for analysis data

--- MPQ-gas-properties/MPQScaling-analysis-scripts/FLAMINGO_MPQScaling_analysis.py
+++ MPQ-gas-properties/MPQScaling-analysis-scripts/FLAMINGO_MPQScaling_analysis.py
@@ -70,17 +70,17 @@ def main():
     # Retrieve binned mass and scaling parameters and define a data frame containing them and save
     scaling_params = mpq_scaling.get_scaling_parameters()
     scaling_params_df = pd.DataFrame(scaling_params)
-    scaling_params_df.to_csv(f"gas_scaling_parameters_{sim_name}_snap00{snap}.csv", index = False)
+    scaling_params_df.to_csv(f"../Data/gas_scaling_parameters_{sim_name}_snap00{snap}.csv", index = False)
 
     # Retrieve binned mass and pairwise property covariance and define a data frame containing them and save
     covs = mpq_scaling.get_covariances()
     covs_df = pd.DataFrame(covs)
-    covs_df.to_csv(f"gas_covariances_{sim_name}_snap00{snap}.csv", index = False)
+    covs_df.to_csv(f"../Data/gas_covariances_{sim_name}_snap00{snap}.csv", index = False)
 
     # Retrieve binned mass and pairwise property correlations and define a data frame containing them and save
     corrs = mpq_scaling.get_correlations()
     corrs_df = pd.DataFrame(corrs)
-    corrs_df.to_csv(f"gas_correlations_{sim_name}_snap00{snap}.csv", index = False)
+    corrs_df.to_csv(f"../Data/gas_correlations_{sim_name}_snap00{snap}.csv", index = False)
     
     # Calculate the MPQs for the individual properties in 'props'
     mpq_individual = mpq_scaling.get_mpq(num_props_in_combination = 1)
@@ -94,7 +94,7 @@ def main():
     mpq_combo_df.drop("M_500c", axis = 1, inplace = True)
     # Merge the the individual MPQ data frame with the combined MPQ data frame and save
     mpq_df = mpq_ind_df.merge(mpq_combo_df, on = "bin")
-    mpq_df.to_csv(f"gas_MPQ_{sim_name}_snap00{snap}.csv", index = False)
+    mpq_df.to_csv(f"../Data/gas_MPQ_{sim_name}_snap00{snap}.csv", index = False)
 
 
 if __name__ == "__main__":
